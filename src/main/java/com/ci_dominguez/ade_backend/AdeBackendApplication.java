ffe09f0d446d01d13005273662dfce58edaba35f
Cristian <cdom127@wgu.edu>
2024-06-19 15:55:33 -0700

    Initial

---
+++ src/main/java/com/ci_dominguez/ade_backend/AdeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ci_dominguez.ade_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ci_dominguez/ade_backend/AdeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ci_dominguez.ade_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
