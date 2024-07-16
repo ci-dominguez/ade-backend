8fa979ad153c1b64c75dc41d7bef366340f684b8
Cristian <cdom127@wgu.edu>
2024-07-16 11:42:25 -0700

    New 1 arg method

--- src/main/java/com/ci_dominguez/ade_backend/exception/DatabaseException.java
+++ src/main/java/com/ci_dominguez/ade_backend/exception/DatabaseException.java
@@ -1,7 +1,11 @@
 package com.ci_dominguez.ade_backend.exception;
 
-public class DatabaseException extends RuntimeException{
-    public DatabaseException(String message, Throwable cause){
+public class DatabaseException extends RuntimeException {
+    public DatabaseException(String message, Throwable cause) {
         super(message, cause);
+    }
+
+    public DatabaseException(String message) {
+        super(message);
     }
 }
