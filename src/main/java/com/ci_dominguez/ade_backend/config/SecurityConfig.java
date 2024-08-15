8ae2894ce9eef143bcebaba6ce5eba20d0d2437e
Cristian <cdom127@wgu.edu>
2024-08-15 10:55:06 -0700

    Update security config to allow hosted frontend url

--- src/main/java/com/ci_dominguez/ade_backend/config/SecurityConfig.java
+++ src/main/java/com/ci_dominguez/ade_backend/config/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        configuration.setAllowedOrigins(Arrays.asList("https://ade.cicdominguez.com"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Auth-Token"));
         configuration.setExposedHeaders(Arrays.asList("X-Auth-Token"));
