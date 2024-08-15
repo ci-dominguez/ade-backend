4a6aa39a69d9ae882045b22511b358e0e6b872c3
Cristian <cdom127@wgu.edu>
2024-08-14 22:16:04 -0700

    globally enable cors for more testing on mobile

--- src/main/java/com/ci_dominguez/ade_backend/AdeBackendApplication.java
+++ src/main/java/com/ci_dominguez/ade_backend/AdeBackendApplication.java
@@ -2,6 +2,9 @@ package com.ci_dominguez.ade_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class AdeBackendApplication {
@@ -10,4 +13,17 @@ public class AdeBackendApplication {
 		SpringApplication.run(AdeBackendApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:5173", "http://10.0.0.9:5173")
+						.allowedMethods("GET", "POST", "PUT", "DELETE")
+						.allowedHeaders("*")
+						.allowCredentials(true);
+			}
+		};
+	}
 }
