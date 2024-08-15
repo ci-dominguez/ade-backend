bc5e66391b06e2a87549185abda55e27554cea13
Cristian <cdom127@wgu.edu>
2024-08-14 23:24:02 -0700

    remove redundant cors bean

--- src/main/java/com/ci_dominguez/ade_backend/AdeBackendApplication.java
+++ src/main/java/com/ci_dominguez/ade_backend/AdeBackendApplication.java
@@ -2,28 +2,11 @@ package com.ci_dominguez.ade_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class AdeBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AdeBackendApplication.class, args);
-	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-						.allowedOrigins("http://localhost:5173", "http://10.0.0.9:5173")
-						.allowedMethods("GET", "POST", "PUT", "DELETE")
-						.allowedHeaders("*")
-						.allowCredentials(true);
-			}
-		};
 	}
 }
